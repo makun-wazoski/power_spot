bc5d2e3b00f66904c222782b013587f846293b2d
makun-wazoski <mo19850226@gmail.com>
2020-10-22 12:12:54 +0900

    新規投稿に成功

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,18 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post=Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :url, :impressions)
+  end
 end
