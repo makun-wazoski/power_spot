2944fc5d7e60993ff6e25e4d7f1857d9fd307c11
makun-wazoski <mo19850226@gmail.com>
2020-10-23 12:37:02 +0900

    Revert "画像投稿機能　追加"
    This reverts commit b68c7bb7aa5ffa4a777f3de52152fd63371473a0.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,3 @@
 class Post < ApplicationRecord
-  has_one_attached :image
-
-  with_options presence: true do
-    validates :name
-    validates :image
-  end
+  validates :name, presence: true
 end
