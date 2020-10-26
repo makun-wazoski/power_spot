6de96689ecd5709ef89b517ece34dff238b9d21a
makun-wazoski <mo19850226@gmail.com>
2020-10-26 14:18:32 +0900

    詳細画面　削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,11 @@ class PostsController < ApplicationController
     @post= Post.find(params[:id])
   end 
 
+  def destroy
+    @post=Post.find(params[:id])
+    @post.destroy
+  end
+  
   private
   def post_params
     params.require(:post).permit(:image, :name, :url, :impressions).merge(user_id: current_user.id)
