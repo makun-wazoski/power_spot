1d278b2a5d4eb776afd9af730f4394334ca04556
makun-wazoski <mo19850226@gmail.com>
2020-10-27 09:11:30 +0900

    編集→更新画面遷移

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,18 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post=Post.find(params[:id]) 
+  end
+
+  def update
+    # @post= Post.find(params[:id])
+    # binding.pry
+    @post=Post.find(params[:id]) 
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
   end
 
   private
