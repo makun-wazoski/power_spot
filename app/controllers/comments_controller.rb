eac6110410b0684dbcc97d7535be2a8a0f1f6d0c
makun-wazoski <mo19850226@gmail.com>
2020-10-30 17:00:47 +0900

    バリデーション（空コメNG）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     comment=Comment.create(comment_params)
-    redirect_to "/posts/#{comment.post.id}"
+    redirect_to post_path(comment.post.id)
   end
 
   private
