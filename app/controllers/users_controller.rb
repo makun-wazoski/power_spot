391b68bb7d7aa1d905bd67543d26594364411c35
makun-wazoski <mo19850226@gmail.com>
2020-11-01 12:57:33 +0900

    プロフィール編集画面　完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @nickname= @user.nickname
     @posts = @user.posts
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 end
