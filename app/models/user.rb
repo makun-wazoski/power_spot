da81e851498e0d4f66698f6637fbb58e2d367471
makun-wazoski <mo19850226@gmail.com>
2020-11-02 19:22:57 +0900

    バリデーション&エラーハンドリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,11 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one_attached :image, dependent: :destroy
          
-
+  with_options presence: true do
+    validates :nickname
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  end
+  validates :favorite_spot, presence: false, length: {maximum: 200 }
+  validates :introduction, presence: false, length: {maximum: 1000 }
+  
 end
