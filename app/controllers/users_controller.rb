f73fa14942cc3c63fbdceb55657a481100caa66c
makun-wazoski <mo19850226@gmail.com>
2020-11-02 15:23:17 +0900

    プロフィール更新画面遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,17 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user=User.find(params[:id])
+    if user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:image, :email, :nickname, :introduction, :favorit_spot).merge(user_id: current_user.id)
+  end
 end
