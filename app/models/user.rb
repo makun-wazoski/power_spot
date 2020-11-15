3efc036c7b4dab8556c4920748a5a2399d6e1f8c
makun-wazoski <mo19850226@gmail.com>
2020-11-15 14:14:33 +0900

    ユーザーモデル　単体テスト　完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_one_attached :image, dependent: :destroy
          
   with_options presence: true do
-    validates :nickname
+    validates :nickname, length: {maximum: 20 }
     validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     validates :password, length: {minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   end
