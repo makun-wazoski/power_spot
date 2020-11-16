370e5f4dc2a13ffdf79a19ade9eca76bed793910
makun-wazoski <mo19850226@gmail.com>
2020-11-16 12:30:58 +0900

    README　作成　完了

--- db/migrate/20201021042500_create_posts.rb
+++ db/migrate/20201021042500_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :name
+      t.string :name,      null: false
       t.text :url
       t.text :impressions
-      t.references :user,         foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
