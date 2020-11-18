a6df2f63b4404ee6fbe116e9bbf4775383bc7cd5
makun-wazoski <mo19850226@gmail.com>
2020-11-18 14:11:21 +0900

    余分なカラムの削除

--- db/migrate/20201101015416_add_columns_to_users.rb
+++ db/migrate/20201101015416_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :image, :string
+    
     add_column :users, :introduction, :text
     add_column :users, :favorite_spot, :string
   end
